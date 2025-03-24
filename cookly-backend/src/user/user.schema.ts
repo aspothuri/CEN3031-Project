3e7adbba2491261ca64e8d9087704f72707a9a4d
Noah Lunberry <nlunberry@gmail.com>
2025-03-23 21:56:28 -0400

    Made sure no duplicate emails and usernames

--- cookly-backend/src/user/user.schema.ts
+++ cookly-backend/src/user/user.schema.ts
@@ -5,11 +5,11 @@ export type UserDocument = HydratedDocument<User>;
 
 @Schema()
 export class User {
-  @Prop({ required: true })
+  @Prop({ required: true, unique: true })
   username: string;
-
-  @Prop({ required: true })
-  email: string;
+  
+  @Prop({ required: true, unique: true })
+  email: string;  
 
   @Prop({ required: true })
   password: string;
