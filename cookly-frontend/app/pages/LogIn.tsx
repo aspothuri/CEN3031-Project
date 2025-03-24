95bf392b0468e45783a07d602e36849367534356
Ryan-slither <ryanpfroug@gmail.com>
2025-03-23 20:32:40 -0400

    Created IP API

--- cookly-frontend/app/pages/LogIn.tsx
+++ cookly-frontend/app/pages/LogIn.tsx
@@ -1,63 +1,74 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import { useRouter } from "expo-router";
 
 export default function LogIn() {
   const router = useRouter();
 
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const LoginFunc = async () => {
-    setErrorMessage(""); 
-  
+    setErrorMessage("");
+
+    console.log("User Logging In");
     try {
-      
-      const response = await fetch("http://localhost:3000/user/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
-      });
-  
+      const response = await fetch(
+        `${process.env.EXPO_PUBLIC_API_URL}/user/login`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email, password }),
+        }
+      );
+
+      console.log(response);
       const data = await response.json();
-  
+
       if (!response.ok) {
-        throw new Error(data.message || "Invalid credentials"); 
+        throw new Error(data.message || "Invalid credentials");
       }
-  
+
       console.log("✅ Login successful:", data);
-  
-       //router.push(`/Profile/username=${data.username}`); // need to fix this once we fix profile page
+
+      //router.push(`/Profile/username=${data.username}`); // need to fix this once we fix profile page
     } catch (error) {
-      const errorMessage = error instanceof Error ? error.message : "Something went wrong";
+      const errorMessage =
+        error instanceof Error ? error.message : "Something went wrong";
       console.error("Login failed:", errorMessage);
-      setErrorMessage(errorMessage); 
+      setErrorMessage(errorMessage);
     }
   };
-  
-  
 
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={require("../../assets/images/cookly-logo.png")} />
+      <Image
+        style={styles.image}
+        source={require("../../assets/images/cookly-logo.png")}
+      />
       <Text style={styles.heading}>Welcome to Cookly</Text>
-      
+
       <View style={styles.inputContainer}>
-        <TextInput 
-          style={styles.input} 
+        <TextInput
+          style={styles.input}
           placeholder="Email"
           value={email}
           onChangeText={setEmail}
           keyboardType="default"
           placeholderTextColor="#555"
         />
-        
-        <TextInput 
-          style={styles.input} 
-          placeholder="Password" 
+
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
           value={password}
           onChangeText={setPassword}
           secureTextEntry
@@ -67,7 +78,9 @@ export default function LogIn() {
       </View>
 
       {/* display Error Message if login fails */}
-      {errorMessage ? <Text style={styles.errorText}>{errorMessage}</Text> : null}
+      {errorMessage ? (
+        <Text style={styles.errorText}>{errorMessage}</Text>
+      ) : null}
 
       <TouchableOpacity style={styles.button} onPress={LoginFunc}>
         <Text style={styles.buttonText}>Submit</Text>
@@ -83,70 +96,70 @@ export default function LogIn() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 16,
-    backgroundColor: '#5be37f',
+    backgroundColor: "#5be37f",
   },
   image: {
     height: 180,
     width: 180,
-    position: 'absolute',
+    position: "absolute",
     top: 60,
     borderRadius: 20,
   },
   heading: {
     fontSize: 28,
-    fontWeight: 'bold',
-    position: 'absolute',
+    fontWeight: "bold",
+    position: "absolute",
     top: 260,
-    color: '#fff',
-    textShadowColor: 'rgba(0, 0, 0, 0.2)',
+    color: "#fff",
+    textShadowColor: "rgba(0, 0, 0, 0.2)",
     textShadowOffset: { width: 1, height: 2 },
     textShadowRadius: 3,
   },
   inputContainer: {
-    width: '85%',
+    width: "85%",
     marginTop: 320,
   },
   input: {
     height: 50,
-    borderColor: '#fff',
+    borderColor: "#fff",
     borderWidth: 2,
     marginBottom: 16,
-    width: '100%',
+    width: "100%",
     paddingLeft: 12,
     borderRadius: 10,
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
     fontSize: 16,
   },
   button: {
-    backgroundColor: '#02b33a',
+    backgroundColor: "#02b33a",
     borderRadius: 25,
     paddingVertical: 14,
     paddingHorizontal: 40,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     marginTop: 10,
-    shadowColor: 'black',
+    shadowColor: "black",
     shadowOffset: { width: 2, height: 3 },
     shadowOpacity: 0.2,
     shadowRadius: 4,
   },
   buttonText: {
-    color: 'white',
+    color: "white",
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   linkText: {
     color: "#fff",
     textDecorationLine: "underline",
     marginTop: 20,
     fontSize: 16,
-    fontWeight: 'bold',
-    textShadowColor: 'rgba(0, 0, 0, 0.2)',
+    fontWeight: "bold",
+    textShadowColor: "rgba(0, 0, 0, 0.2)",
     textShadowOffset: { width: 1, height: 1 },
     textShadowRadius: 2,
   },
-  errorText: { color: 'red', fontSize: 16, marginTop: 10 },
+  errorText: { color: "red", fontSize: 16, marginTop: 10 },
 });
---
+++ cookly-frontend/app/pages/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+
+export default function Layout() {
+  return (
+    <Stack>
+      <Stack.Screen name="LogIn" options={{}} />
+      <Stack.Screen name="Profile" options={{}} />
+      <Stack.Screen name="SignUp" options={{}} />
+    </Stack>
+  );
+}
